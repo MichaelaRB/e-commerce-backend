ff9ecc7341ca77690d352dcbe771332d61a95f5b
Michaela Brown <michaelarbrown4@gmail.com>
2023-06-14 15:37:36 -0500

    removed test code from the tag-routes.js file, finished the get and delete routes for product-routes.js

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -5,7 +5,6 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 router.get('/', async (req, res) => {
   // find all tags
-  // be sure to include its associated Product data
   try {
     const tagData = await Tag.findAll({include: [{ model: Product, through: ProductTag, as: 'product_tags'}]});
     res.status(200).json(tagData);
@@ -16,11 +15,11 @@ router.get('/', async (req, res) => {
 });
 
 router.get('/:id', async (req, res) => {
+  //find specified tag by id
   try {
     const tagData = await Tag.findByPk(req.params.id, {
       include: [{ model: Product, through: ProductTag, as: 'product_tags'}]
     });
-    console.log(tagData);
     if (!tagData) {
       res.status(404).json({ message: 'There is no tag with this id!' });
       return;
@@ -33,9 +32,10 @@ router.get('/:id', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
+  //create a new tag
   try {
     const tagData = await Tag.create(req.body);
-    res.status(200).json(locationData);
+    res.status(200).json(tagData);
   } catch (err) {
     res.status(400).json(err);
   }
@@ -73,7 +73,7 @@ router.delete('/:id', async (req, res) => {
       return;
     }
 
-    res.status(200).json(locationData);
+    res.status(200).json(tagData);
   } catch (err) {
     res.status(500).json(err);
   }
